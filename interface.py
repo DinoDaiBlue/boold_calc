04605eae2ff0340e27f63c19dcf8afc7ef301695
Xiang Dai <dino@DinodeMacBook-Pro.local>
2020-08-29 22:24:02 -0400

    create a HDL_analysis by importing the functions from HDL

--- interface.py
+++ interface.py
@@ -1,10 +1,14 @@
+import HDL
 def interface():
 	while True:
 		print("My Program")
 		print("Options: ")
 		print("9 - Quit")
+		print("1 - HDL_Analysis")
 		choice = input("Enter your choice: ")
-		if choice=='9':
+		if choice =='9':
 			break
-			return
+		if choice =='1':
+			value, mes = driver()			
+			break
 interface()
