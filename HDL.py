8a547f4dda10624ecedb77f5ed3c4c8ab3d3b8b2
Xiang Dai <dino@DinodeMacBook-Pro.local>
2020-08-29 21:52:04 -0400

    Add a function to check the HDL situation and print the value and message

--- HDL.py
+++ HDL.py
@@ -2,5 +2,16 @@ def user_input():
 	HDL_val = input("Please enter your HDl value here: ")
 	HDL_num = float(HDL_val)
 	return HDL_num
-
-user_input()
+def check_HDL(HDL_num):
+	if HDL_num >= 60:
+		mes = "Normal"
+		print("HDL value is {}, it is {}".format(HDL_num, mes))
+	elif 40 <= HDL_num < 60:
+		mes = "Borderline Low"
+		print("HDL value is {}, it is {}".format(HDL_num, mes))
+	else: 
+		mes = "Low"
+		print("HDL value is {}, it is {}".format(HDL_num, mes))
+	return  
+HDL_num = user_input()
+check_HDL(HDL_num)
